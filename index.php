e890e1e25d88d743b3c89a62fd8764fdc2390c8a
sleepyboo <thunderlrod520@gmail.com>
2021-01-03 18:14:59 -0500

    made it work ishh

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ require_once('vendor/autoload.php');
 \Dotenv\Dotenv::createImmutable(__DIR__)->load();    
 //typings
 use Discord\Discord;
+use Discord\Parts\Channel\Message;
 use Discord\Parts\User\Member;
 use Discord\WebSockets\Event;
 use Discord\WebSockets\Intents;
+
+use function React\Promise\Stream\first;
+
 //discord client creations
 $discord = new Discord([
     'token' => $_ENV["DISCORD_TOKEN"],
@@ -20,22 +24,69 @@ $discord = new Discord([
     ],
     ]);
 //DB setup
+
 $db = new SQLite3('records.db');
-$db->query('CREATE TABLE IF NOT EXISTS reputatations(
+$db->query('CREATE TABLE IF NOT EXISTS reputations(
     id int primary key,
     reputation int
 )');
-
 $discord->on('ready', function ($discord) {
 	echo "Bot is ready!", PHP_EOL;
 
 	// Listen for messages.
 	$discord->on('message', function ($message, $discord) {
-        echo "{$message->author->username}: {$message->content}",PHP_EOL;
-        if(!$message->author->user->bot){$message->reply("hi there");}
+        if(
+           // !$message->author->user->bot&&
+         explode(" ", $message->content)[0]=="rep" 
+        && count($message->mentions)==1 
+        //&& (explode(" ", $message->content)[0]=="++"||explode(" ", $message->content)[0]=="--")
+        ){
+            print("yes!\n\n\\n");
+            $cmd = (explode(" ", $message->content));
+            var_dump($cmd);
+            array_shift($cmd); array_shift($cmd);
+            $cmd = array_reverse($cmd);
+            $cmd = array_shift($cmd);
+            var_dump($cmd);
+            $ids=$message->mentions;
+            $mention;
+            foreach($ids as $f => $val){
+                global $mention;
+                $mention = $val;
+            }
+            $id=$mention->id;
+            var_dump($id);
+            global $db;
+            $username = $mention->username;
+            $rep = $db->querySingle("SELECT reputation FROM reputations WHERE id={$id}");
+            if($cmd=="++"){
+                if($rep==NULL){
+                     $db->query("INSERT INTO reputations VALUES({$id},1)");
+                     $message->reply("set {$username}'s rep to 1");
+                     var_dump("does not exist");
+                }else{
+                    $rep+=1;
+                    $db->query("UPDATE reputations SET reputation={$rep} WHERE id={$id}");
+                    $message->reply("set {$username}'s rep to {$rep}");
+                    var_dump("does exist");
+                }
+               
+            }else if($cmd=="--"){
+                if($rep==NULL){
+                     $db->query("INSERT INTO reputations VALUES({$id},{-1})");
+                     $message->reply("set {$username}'s rep to -1");
+                }else{
+                    $rep-=1;
+                    $db->query("UPDATE reputations SET reputation={$rep} WHERE id={$id}");
+                    $message->reply("set {$username}'s rep to {$rep}");
+                }
+            }
+
+        }
         # trying to get message mentions to get user object and get ID to add to DB. 
         # discord usernames/tags can change, but IDs cannot, so the ID will go in the DB
-        // $d = array_key_first($message->mentions);
+        
+        //ar_dump(array_shift($message->mentions));
         // $message->channel->sendMessage("{$d}");
     });
     $discord->on(Event::GUILD_MEMBER_ADD, function (Member $member, Discord $discord) {
