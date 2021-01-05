dfdfb127d6f593c3f9a5f42d42aeb9cf3e16f609
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2021-01-05 19:37:34 +0100

    add comment

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -3,20 +3,20 @@ import dotenv from "dotenv"
 import { PrismaClient } from "@prisma/client"
 
 dotenv.config()
-const client = new Discord.Client()
+const client = new Discord.Client() 
 const prisma = new PrismaClient()
 
 async function repUpdate(userId: number, operation: string): Promise<number> {
-  const exists = await prisma.user.findUnique({where: {id: userId}})
+  const exists = await prisma.user.findUnique({where: {id: userId}}) //check if the user already exists 
   if(!exists) {
-    operation.includes("++") ?
+    operation.includes("++") ? // create the user and add rep 0 or 2 depeniing if the contents includes ++
       await prisma.user.create({data: { id: userId, rep: 2 }}) :
       await prisma.user.create({data: { id: userId, rep: 0 }})
-    return operation.includes("++") ? 2 : 0
+    return operation.includes("++") ? 2 : 0 // return rep for message
   } else {
-    let user = await prisma.user.findUnique({where: {id: userId}})
+    let user = await prisma.user.findUnique({where: {id: userId}}) // get the user
     operation.includes("++") ?
-      await prisma.user.update({where: {id: userId}, data: { rep: Number(user!.rep + 1) }}) :
+      await prisma.user.update({where: {id: userId}, data: { rep: Number(user!.rep + 1) }}) : // update the user
       await prisma.user.update({where: {id: userId}, data: { rep: Number(user!.rep - 1) }})
     return operation.includes("++") ?
       user!.rep + 1 :
@@ -29,29 +29,29 @@ client.on('ready', () => {
 });
 
 client.on('message', async (msg) => {
-  const args: any[] = msg.content.trim().split(' ')
+  const args: any[] = msg.content.trim().split(' ') // get args
   if (
-      msg.content.includes("++") || msg.content.includes("--")
+      msg.content.includes("++") || msg.content.includes("--") // check if the message contains -- or ++
   ) {
-    let targetName: string = args.join(" ").replace(/(\+\+|--)\s*$/, "")
-    if(targetName.includes('@')) {
-      const pingedUsed: Object = msg.mentions.users.toJSON()
-      const userId = Number(pingedUsed[0].id)
+    let targetName: string = args.join(" ").replace(/(\+\+|--)\s*$/, "") // get the name
+    if(targetName.includes('@')) { // check if the name has a @ in it
+      const pingedUsed: Object = msg.mentions.users.toJSON() // get the mentioned user
+      const userId = Number(pingedUsed[0].id) // get the user id
       await msg.channel.send(`${pingedUsed[0].username} has rep ${await repUpdate(userId, msg.content)}`)
       return
     }
 
-    let target = await msg.guild?.members.fetch()
-    let user, userName
+    let target = await msg.guild?.members.fetch() // get all members form the server
+    let user, userName 
 
     for(const [key, e] of target!.entries()) {
-      if(e.nickname?.includes(targetName)) {user = e; userName = e.nickname; break}
-      if(e.user.username?.includes(targetName)) {user = e; userName = e.user.username; break}
+      if(e.nickname?.includes(targetName)) {user = e; userName = e.nickname; break} // if nickname matches the member break from loop
+      if(e.user.username?.includes(targetName)) {user = e; userName = e.user.username; break} // if username matches the member break from loop
     }
 
-    if(!user || !userName) {msg.channel.send("Can't find user"); return}
+    if(!user || !userName) {msg.channel.send("Can't find user"); return} // check if the user and username exist
 
-    const userId = Number(user.id)
+    const userId = Number(user.id) // get the user id
     await msg.channel.send(`${userName} has rep ${await repUpdate(userId, msg.content)}`)
   }
 });
